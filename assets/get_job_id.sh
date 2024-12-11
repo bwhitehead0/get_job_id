525df0f1e084b001555f84450a0af7fbccc776f3
bwhitehead0 <whitehead.brandon@gmail.com>
2024-12-11 10:18:33 -0600

    update yaml and script to use inputs w/ default values set to github context variables

--- assets/get_job_id.sh
+++ assets/get_job_id.sh
@@ -3,6 +3,6 @@
 # this uses the builtin github variable github.job that identifies the job name, and uses that to filter the response to get the corresponding job id. we must normalize the job name to lowercase to ensure the filter works as expected, as github returns the job name in sentence case.
 echo $(curl -L -s \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+  -H "Authorization: Bearer ${INPUT_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "${{ github.server_url }}/api/v3/repos/${{ github.repository }}/actions/runs/${{ github.run_id }}/jobs" | jq --arg name "${{ github.job }}" '.jobs[] | select(.name | ascii_downcase == ($name | ascii_downcase)) | .id')
+  "${INPUT_SERVER_URL}/api/v3/repos/${INPUT_REPOSITORY}/actions/runs/${INPUT_RUN_ID}/jobs" | jq --arg name "${INPUT_JOB}" '.jobs[] | select(.name | ascii_downcase == ($name | ascii_downcase)) | .id')
