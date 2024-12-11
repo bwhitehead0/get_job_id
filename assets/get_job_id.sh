07c7874a4c44186907f937c94c18767c4af4466e
bwhitehead0 <whitehead.brandon@gmail.com>
2024-12-11 11:51:35 -0600

    removed commented out inputs. explicitly set job ID result to GITHUB_OUTPUT

--- assets/get_job_id.sh
+++ assets/get_job_id.sh
@@ -6,8 +6,12 @@ response=$(curl -L -s \
   "${GITHUB_SERVER_URL}/api/v3/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs")
 
 if [ "$(echo "$response" | jq '.jobs')" != "null" ]; then
-  echo "$response" | jq --arg name "${GITHUB_JOB}" '.jobs[] | select(.name | ascii_downcase == ($name | ascii_downcase)) | .id'
+  VALUE=$(echo "$response" | jq --arg name "${GITHUB_JOB}" '.jobs[] | select(.name | ascii_downcase == ($name | ascii_downcase)) | .id')
 else
   echo "Error: Unable to retrieve jobs. Please check your inputs and try again."
   exit 1
 fi
+
+echo $VALUE
+# set to github output
+echo "job_id=$VALUE" >> $GITHUB_OUTPUT
