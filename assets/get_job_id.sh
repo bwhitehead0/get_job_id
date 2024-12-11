38358b48c73f877c4ac5e351048e0768d9432eda
bwhitehead0 <whitehead.brandon@gmail.com>
2024-12-11 11:35:30 -0600

    working passing token via workflow env: block. removing debug output. added exit 1

--- assets/get_job_id.sh
+++ assets/get_job_id.sh
@@ -9,18 +9,5 @@ if [ "$(echo "$response" | jq '.jobs')" != "null" ]; then
   echo "$response" | jq --arg name "${GITHUB_JOB}" '.jobs[] | select(.name | ascii_downcase == ($name | ascii_downcase)) | .id'
 else
   echo "Error: Unable to retrieve jobs. Please check your inputs and try again."
+  exit 1
 fi
-# TK=${GITHUB_TOKEN}
-# echo "DEBUG OUTPUT::::"
-# echo "URL: ${GITHUB_SERVER_URL}"
-# echo "REPOSITORY: ${GITHUB_REPOSITORY}"
-# echo "RUN_ID: ${GITHUB_RUN_ID}"
-# echo "JOB: ${GITHUB_JOB}"
-# echo "TOKEN: ${TK}"
-# echo "TOKEN: ${GITHUB_TOKEN}"
-
-# echo "ENV:::::"
-# env | sort
-# echo "==================================="
-# echo "RESPONSE::::"
-# echo "${response}"
