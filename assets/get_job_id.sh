807f874300138eb8521d4e8afa8cd07227d1586e
bwhitehead0 <whitehead.brandon@gmail.com>
2025-02-06 12:57:12 -0600

    added debug output options

--- assets/get_job_id.sh
+++ assets/get_job_id.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+DEBUG="false"
+
+while getopts "D:" opt; do
+  case $opt in
+    D)
+      DEBUG="${OPTARG}"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 response=$(curl -L -s \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   "${GITHUB_SERVER_URL}/api/v3/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs")
+
+
+if [ "$DEBUG" = "true" ]; then
+  echo echo "Raw response: $response"
+fi
 
 if [ "$(echo "$response" | jq '.jobs')" != "null" ]; then
   VALUE=$(echo "$response" | jq --arg name "${GITHUB_JOB}" '.jobs[] | select(.name | ascii_downcase == ($name | ascii_downcase)) | .id')
